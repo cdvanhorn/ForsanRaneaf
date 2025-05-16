34baa599dc68a22cdaa4bcf36ce8e11998d497d2
cdvanhorn <david@zorttroz.com>
2025-05-16 18:52:07 -0400

    updated esp32 controller program so I'll be able to test whole application on local

--- SensorNetwork/controller/controller.ino
+++ SensorNetwork/controller/controller.ino
@@ -1,29 +1,44 @@
-#define RXD2 16
-#define TXD2 17
-#define GPS_BAUD 115200
+#include "controller_config.h"
 
 HardwareSerial infotainmentSerial(2);
 
+bool isGPIOSerial() {
+  return strcmp(SERIAL_OUTPUT, SERIAL_GPIO) == 0;
+}
+
+void readSerial() {
+  if(isGPIOSerial()) {
+    while(infotainmentSerial.available() > 0) {
+      char data = infotainmentSerial.read();
+    }
+  } else {
+    while(Serial.available() > 0) {
+      char data = Serial.read();
+    }
+  }
+}
+
+void writeSerial() {
+  if(isGPIOSerial()) {
+    infotainmentSerial.print("the sensor network controller says hello");
+  } else {
+    Serial.print("the sensor network controller says hello");
+  }
+}
+
 void setup() {
   // Serial Monitor
-  Serial.begin(115200);
+  Serial.begin(SERIAL_BAUD);
   
-  // Start Serial 2 with the defined RX and TX pins and a baud rate of 9600
-  infotainmentSerial.begin(GPS_BAUD, SERIAL_8N1, RXD2, TXD2);
-  Serial.println("Serial 2 started at 115200 baud rate");
+  if(isGPIOSerial()) {
+    // Start Serial 2 with the defined RX and TX pins
+    infotainmentSerial.begin(SERIAL_BAUD, SERIAL_8N1, RXD2, TXD2);
+    Serial.println("GPIO serial started");
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  infotainmentSerial.println("the sensor network controller says hello");
-  Serial.println("sent data on uart");
-
-  Serial.println("reading uart");
-  while(infotainmentSerial.available() > 0) {
-    char data = infotainmentSerial.read();
-    Serial.print(data);
-  }
-
-  Serial.println("\nsleeping");
-  delay(1000);
+  writeSerial();
+  readSerial();
+  delay(2000);
 }
---
+++ SensorNetwork/controller/controller_config.h
@@ -0,0 +1,10 @@
+
+#define RXD2          16
+#define TXD2          17
+#define SERIAL_BAUD   115200
+
+// should we output serial messages over USB or GPIO
+// valid settings GPIO or USB
+#define SERIAL_OUTPUT   "USB"
+#define SERIAL_USB      "USB"
+#define SERIAL_GPIO     "GPIO"

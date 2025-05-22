436037240bbec0541edde23a7fad6111f70d555d
cdvanhorn <david@zorttroz.com>
2025-05-22 19:42:23 -0400

    config loaded on startup

--- Infotainment/main.c
+++ Infotainment/main.c
@@ -9,21 +9,14 @@
 #include <unistd.h>
 #include <termios.h>
 
-/*static void config_game(struct game_cfg *cfg)
-{
-	inicfg_getstring("game", "core", &(cfg->core));
-	inicfg_getstring("input", "core", &(cfg->input_core));
-	inicfg_getuint8_t("input", "cps", &(cfg->input_cps));
-	inicfg_getstring("render", "core", &(cfg->render_core));
-	inicfg_getuint8_t("render", "fps", &(cfg->render_fps));
-	inicfg_getstring("render", "renderer", &(cfg->render_renderer));
-	inicfg_getstring("audio", "core", &(cfg->audio_core));
-	inicfg_getuint8_t("audio", "cps", &(cfg->audio_cps));
-	inicfg_getuint8_t("simulation", "cps", &(cfg->sim_cps));
-}*/
-
 static void config_frinfo(struct frinfo_config *cfg) {
-
+#ifdef RPI
+	inicfg_getstring("serial", "pipath", &(cfg->serial_path));
+#else
+	inicfg_getstring("serial", "path", &(cfg->serial_path));
+#endif
+	inicfg_getint("serial", "baud", &(cfg->serial_baud_rate));
+	inicfg_getint("serial", "buffer_size", &(cfg->serial_buffer_size));
 }
 
 int main(int argc, char *argv[])
@@ -34,11 +27,6 @@ int main(int argc, char *argv[])
 	log_write(LOG_TAG_INFO, "logger initialized");
 
 	inicfg_open();
-
-	// open serial reading thread
-	// char *setting;
-	// inicfg_getstring("serial", "path", &setting);
-	// open UI thread
 
 	struct frinfo_config frinfocfg;
 	config_frinfo(&frinfocfg);

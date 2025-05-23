626c23a3c126fc5a8e9a685ddb2d959c94e67d08
cdvanhorn <david@zorttroz.com>
2025-05-22 21:06:02 -0400

    removed commented out code

--- Infotainment/main.c
+++ Infotainment/main.c
@@ -2,13 +2,6 @@
 #include "utilities/logger.h"
 #include "frinfo/frinfo.h"
 
-#include <stddef.h>
-
-#include <stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <termios.h>
-
 static void config_frinfo(struct frinfo_config *cfg) {
 #ifdef RPI
 	inicfg_getstring("serial", "pipath", &(cfg->serial_path));
@@ -37,49 +30,4 @@ int main(int argc, char *argv[])
 	log_close();
 	log_write(LOG_TAG_INFO, "logger destroyed");
 	return 0;
-
-	// int fd;
-	// char buffer[256];
-	// struct termios tty;
-	//
-	// // Open the serial port
-	// fd = open("/dev/ttyAMA0", O_RDWR | O_NOCTTY);
-	// if (fd < 0) {
-	// 	perror("Error opening serial port");
-	// 	return 1;
-	// }
-	//
-	// // Get the current serial port settings
-	// if (tcgetattr(fd, &tty) != 0) {
-	// 	perror("Error getting serial port settings");
-	// 	close(fd);
-	// 	return 1;
-	// }
-	//
-	// // Configure the serial port
-	// tty.c_cflag = B115200 | CS8 | CLOCAL | CREAD; // Set baud rate, data bits, and enable receiver
-	// tty.c_iflag = IGNPAR; // Ignore parity errors
-	// tty.c_oflag = 0; // Raw output
-	// tty.c_lflag = 0; // Non-canonical mode
-	//
-	// // Apply the new settings
-	// if (tcsetattr(fd, TCSANOW, &tty) != 0) {
-	// 	perror("Error setting serial port settings");
-	// 	close(fd);
-	// 	return 1;
-	// }
-	//
-	// // Read data from the serial port
-	// ssize_t bytes_read = read(fd, buffer, sizeof(buffer));
-	// if (bytes_read < 0) {
-	// 	perror("Error reading from serial port");
-	// 	close(fd);
-	// 	return 1;
-	// }
-	//
-	// // Print the received data
-	// printf("Received data: %.*s\n", (int)bytes_read, buffer);
-	//
-	// // Close the serial port
-	// close(fd);
 }

0c95c20deb99db49288226658b5f01b8879a368d
cdvanhorn <david@zorttroz.com>
2025-06-12 22:22:28 -0400

    add methods to start and stop WI-FI that will be needed for ESP-NOW

--- SensorNetwork/battery-sensor/main/battery-sensor_main.c
+++ SensorNetwork/battery-sensor/main/battery-sensor_main.c
@@ -1,8 +1,3 @@
-/*
- * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
- *
- * SPDX-License-Identifier: CC0-1.0
- */
 
 #include <stdio.h>
 #include <inttypes.h>
@@ -10,37 +5,80 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_chip_info.h"
+#include "esp_event.h"
 #include "esp_flash.h"
 #include "esp_system.h"
+#include "esp_netif.h"
+#include "esp_wifi.h"
+#include "nvs_flash.h"
+
+// TODO: Make a config variable
+#define WIFI_PRIMARY_CHANNEL 10
+
+// TODO: Make a config variable
+static bool wifi_long_range = true;
+
+static void start_wifi() {
+    ESP_ERROR_CHECK(esp_netif_init()); // initialize tcp/ip staci
+    ESP_ERROR_CHECK(esp_event_loop_create_default()); // create event loop so can handle tcp/ip events
+    const wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT(); // macro to create default wifi config
+    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) ); // store settings in volatile ram
+    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) ); // put wifi in station mode
+    ESP_ERROR_CHECK( esp_wifi_start()); // start the wifi
+    ESP_ERROR_CHECK( esp_wifi_set_channel(WIFI_PRIMARY_CHANNEL, WIFI_SECOND_CHAN_NONE));
+
+    if (wifi_long_range) {
+        ESP_ERROR_CHECK( esp_wifi_set_protocol(ESP_IF_WIFI_STA,
+            WIFI_PROTOCOL_11B|WIFI_PROTOCOL_11G|WIFI_PROTOCOL_11N|WIFI_PROTOCOL_LR) );
+    }
+}
+
+static void stop_wifi() {
+    ESP_ERROR_CHECK(esp_wifi_stop());
+    ESP_ERROR_CHECK(esp_wifi_deinit());
+}
 
 void app_main(void)
 {
-    printf("Hello world!\n");
+    // Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK( nvs_flash_erase() );
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK( ret );
+
+    start_wifi();
+    printf("WIFI interface started in station mode at 2.4G!\n");
+
+    stop_wifi();
+    printf("WIFI interface stopped!\n");
 
     /* Print chip information */
-    esp_chip_info_t chip_info;
-    uint32_t flash_size;
-    esp_chip_info(&chip_info);
-    printf("This is %s chip with %d CPU core(s), %s%s%s%s, ",
-           CONFIG_IDF_TARGET,
-           chip_info.cores,
-           (chip_info.features & CHIP_FEATURE_WIFI_BGN) ? "WiFi/" : "",
-           (chip_info.features & CHIP_FEATURE_BT) ? "BT" : "",
-           (chip_info.features & CHIP_FEATURE_BLE) ? "BLE" : "",
-           (chip_info.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : "");
-
-    unsigned major_rev = chip_info.revision / 100;
-    unsigned minor_rev = chip_info.revision % 100;
-    printf("silicon revision v%d.%d, ", major_rev, minor_rev);
-    if(esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
-        printf("Get flash size failed");
-        return;
-    }
-
-    printf("%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
-           (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-
-    printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
+    // esp_chip_info_t chip_info;
+    // uint32_t flash_size;
+    // esp_chip_info(&chip_info);
+    // printf("This is %s chip with %d CPU core(s), %s%s%s%s, ",
+    //        CONFIG_IDF_TARGET,
+    //        chip_info.cores,
+    //        (chip_info.features & CHIP_FEATURE_WIFI_BGN) ? "WiFi/" : "",
+    //        (chip_info.features & CHIP_FEATURE_BT) ? "BT" : "",
+    //        (chip_info.features & CHIP_FEATURE_BLE) ? "BLE" : "",
+    //        (chip_info.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : "");
+    //
+    // unsigned major_rev = chip_info.revision / 100;
+    // unsigned minor_rev = chip_info.revision % 100;
+    // printf("silicon revision v%d.%d, ", major_rev, minor_rev);
+    // if(esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
+    //     printf("Get flash size failed");
+    //     return;
+    // }
+    //
+    // printf("%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
+    //        (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+    //
+    // printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
 
     for (int i = 10; i >= 0; i--) {
         printf("Restarting in %d seconds...\n", i);

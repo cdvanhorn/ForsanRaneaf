fb1b18378161f3bf7f8e29686431f69eae532d1a
cdvanhorn <david@zorttroz.com>
2025-06-16 21:59:15 -0400

    working on bootstrapping esp-now network

--- SensorNetwork/battery-sensor/main/battery-sensor_main.c
+++ SensorNetwork/battery-sensor/main/battery-sensor_main.c
@@ -1,6 +1,10 @@
 
+#include <esp_log.h>
+#include <esp_now.h>
 #include <stdio.h>
 #include <inttypes.h>
+#include <string.h>
+
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -13,19 +17,63 @@
 #include "nvs_flash.h"
 
 // TODO: Make a config variable
-#define WIFI_PRIMARY_CHANNEL 10
+#define WIFI_PRIMARY_CHANNEL            10
 
-// TODO: Make a config variable
+#define ESP_NOW_CLIENT_MODE             0
+#define ESP_NOW_CONTROLLER_MODE         1
+
+#define ESP_NOW_COOLANT_CLIENT          0
+#define ESP_NOW_VCU_CLIENT              1
+#define ESP_NOW_BATTERY_CLIENT          2
+
+#define ESP_NOW_QUEUE_SIZE              10
+
+#define ESP_NOW_CONN_STATE_REG          0
+#define ESP_NOW_CONN_STATE_REG_ACK      1
+#define ESP_NOW_CONN_STATE_READY        2
+
+#define ESP_NOW_EVENT_RECEIVE           0
+#define ESP_NOW_EVENT_SEND              1
+
 static bool wifi_long_range = true;
+static uint8_t esp_now_mode = ESP_NOW_CLIENT_MODE;
+static uint8_t esp_now_conn_state = ESP_NOW_CONN_STATE_REG;
+static const char *TAG = "battery_sensor";
+static uint8_t broadcast_mac[ESP_NOW_ETH_ALEN] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+static QueueHandle_t esp_now_queue = NULL;
 
+struct esp_now_send_event{
+    uint8_t mac_addr[ESP_NOW_ETH_ALEN];
+    esp_now_send_status_t status;
+};
+
+struct esp_now_receive_event {
+    uint8_t mac_addr[ESP_NOW_ETH_ALEN];
+    uint8_t *data;
+    int data_len;
+};
+
+union esp_now_event_data {
+    struct esp_now_send_event *send_event;
+    struct esp_now_receive_event *receive_event;
+};
+
+struct esp_now_event {
+    uint8_t event_id;
+    union esp_now_event_data *data;
+};
+
+/**
+ * @brief start the wi-fi driver, needed to create an esp-now network
+ */
 static void start_wifi() {
     ESP_ERROR_CHECK(esp_netif_init()); // initialize tcp/ip staci
     ESP_ERROR_CHECK(esp_event_loop_create_default()); // create event loop so can handle tcp/ip events
     const wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT(); // macro to create default wifi config
     ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
     ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) ); // store settings in volatile ram
-    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) ); // put wifi in station mode
-    ESP_ERROR_CHECK( esp_wifi_start()); // start the wifi
+    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) ); // put wi-fi in station mode
+    ESP_ERROR_CHECK( esp_wifi_start()); // start the wi-fi
     ESP_ERROR_CHECK( esp_wifi_set_channel(WIFI_PRIMARY_CHANNEL, WIFI_SECOND_CHAN_NONE));
 
     if (wifi_long_range) {
@@ -34,9 +82,102 @@ static void start_wifi() {
     }
 }
 
+/**
+ * @brief stop the wi-fi driver and clean up
+ */
 static void stop_wifi() {
     ESP_ERROR_CHECK(esp_wifi_stop());
     ESP_ERROR_CHECK(esp_wifi_deinit());
+}
+
+static void receive_callback(const esp_now_recv_info_t *recv_info, const uint8_t *data, int len) {
+
+}
+
+static void send_callback(const uint8_t *mac_addr, esp_now_send_status_t status) {
+
+}
+
+/**
+ * @brief start esp_now service and any accompanying objects
+ */
+static int start_esp_now() {
+    esp_now_queue = xQueueCreate(ESP_NOW_QUEUE_SIZE, sizeof(struct esp_now_event *));
+    if (esp_now_queue == NULL) {
+        ESP_LOGE(TAG, "Create queue fail");
+        return ESP_FAIL;
+    }
+
+    ESP_ERROR_CHECK( esp_now_init() );
+    ESP_ERROR_CHECK( esp_now_register_send_cb(send_callback) );
+    ESP_ERROR_CHECK( esp_now_register_recv_cb(receive_callback) );
+
+    // add broadcast mac to list of peers so we can use it for bootstrapping
+    esp_now_peer_info_t peer_info;
+    peer_info.channel = WIFI_PRIMARY_CHANNEL;
+    peer_info.ifidx = ESP_IF_WIFI_STA;
+    peer_info.encrypt = false;
+    memcpy(&(peer_info.peer_addr), broadcast_mac, ESP_NOW_ETH_ALEN);
+    ESP_ERROR_CHECK( esp_now_add_peer(&peer_info) );
+
+    // start in register mode
+    esp_now_conn_state = ESP_NOW_CONN_STATE_REG;
+
+    return ESP_OK;
+}
+
+/**
+ * stop esp now service
+ */
+static void stop_esp_now() {
+    esp_now_deinit();
+    if (esp_now_queue != NULL) {
+        vQueueDelete(esp_now_queue);
+        esp_now_queue = NULL;
+    }
+}
+
+static int esp_now_controller_process() {
+    // process each event on the queue
+    // if send event check for failures and potentially resend messages
+        // if register_ack and success mark client as registered
+    // if receive event
+        // unicast register message and not registered, register the peer
+        // send unicast register_ack message to client
+
+    // register mode
+    // if not all the clients have checked in (received unicast register and register_ack sent successfully),
+        // send broadcast register message
+    // if all have checked in change mode to ready
+
+    struct esp_now_event *event;
+    while (xQueueReceive(esp_now_queue, &event, 0) == pdTRUE) {
+        // if (event->event_id == ESP_NOW_EVENT_SEND) {
+        //
+        // } else if (event->event_id == ESP_NOW_EVENT_RECEIVE) {
+        //
+        // }
+    }
+
+    return ESP_OK;
+}
+
+static int esp_now_client_process() {
+    // process each event in the queue
+    // if send event check for failures and log them and potentially resend them
+    // if receive event
+        // register broadcast message in register mode, add to peer list, register-ack mode
+            // send unicast register message to controller
+        // register-ack unicast message, change to ready mode
+
+    return ESP_OK;
+}
+
+static int esp_now_process() {
+    if (esp_now_mode == ESP_NOW_CLIENT_MODE)
+        return esp_now_client_process();
+    if (esp_now_mode == ESP_NOW_CONTROLLER_MODE)
+        return esp_now_controller_process();
 }
 
 void app_main(void)
@@ -50,10 +191,16 @@ void app_main(void)
     ESP_ERROR_CHECK( ret );
 
     start_wifi();
-    printf("WIFI interface started in station mode at 2.4G!\n");
+    ESP_LOGI(TAG, "WIFI interface started in station mode at 2.4G!");
+
+    start_esp_now();
+    ESP_LOGI(TAG, "ESP-NOW started!");
+
+    stop_esp_now();
+    ESP_LOGI(TAG, "ESP_NOW stopped!");
 
     stop_wifi();
-    printf("WIFI interface stopped!\n");
+    ESP_LOGI(TAG, "WIFI interface stopped!");
 
     /* Print chip information */
     // esp_chip_info_t chip_info;
